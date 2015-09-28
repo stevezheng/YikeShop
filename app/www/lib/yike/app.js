bda98b5641044b6788a3e936b528ec46cd5115b9
stevezheng <stevezheng1989@gmail.com>
2015-09-29 01:53:03 +0800

    更新 controller和路由

--- app/www/lib/yike/app.js
+++ app/www/lib/yike/app.js
@@ -96,3 +96,30 @@ angular.module('starter', ['ionic', 'home.controller'])
     $urlRouterProvider.otherwise('/tab/home');
 
   });
+
+(function () {
+  'use strict';
+
+  angular
+    .module('home.controller', [])
+    .controller('HomeCtrl', HomeCtrl);
+
+  HomeCtrl.$inject = ['$scope'];
+
+  /* @ngInject */
+  function HomeCtrl($scope) {
+    $scope.banners = [
+      {image: 'http://ac-i8LBVLYg.clouddn.com/0452828c615807ba.jpg'},
+      {image: 'http://ac-i8LBVLYg.clouddn.com/98eac1120bcd5534.jpg'},
+      {image: 'http://ac-i8LBVLYg.clouddn.com/5fb3482ff886816f.jpg'},
+      {image: 'http://ac-i8LBVLYg.clouddn.com/60fd14fa7a1bdf5f.jpg'},
+      {image: 'http://ac-i8LBVLYg.clouddn.com/86b15c91915cb6ff.jpg'}
+    ];
+    init();
+
+    ////////////////
+
+    function init() {
+    }
+  }
+})();
