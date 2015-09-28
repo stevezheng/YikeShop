bc0380792244173cd85a575009fbbc5d9a174499
stevezheng <stevezheng1989@gmail.com>
2015-09-29 01:24:52 +0800

    更新 tabs

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -46,31 +46,38 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
       // Each tab has its own nav history stack:
 
-      .state('tab.dash', {
-        url: '/dash',
+      .state('tab.home', {
+        url: '/home',
         views: {
-          'tab-dash': {
-            templateUrl: 'templates/tab-dash.html',
-            controller: 'DashCtrl'
+          'tab-home': {
+            templateUrl: 'templates/tab-home.html'
           }
         }
       })
 
-      .state('tab.chats', {
-        url: '/chats',
+      .state('tab.category', {
+        url: '/category',
         views: {
-          'tab-chats': {
-            templateUrl: 'templates/tab-chats.html',
-            controller: 'ChatsCtrl'
+          'tab-category': {
+            templateUrl: 'templates/tab-category.html'
           }
         }
       })
-      .state('tab.chat-detail', {
-        url: '/chats/:chatId',
+
+      .state('tab.dash', {
+        url: '/dash',
         views: {
-          'tab-chats': {
-            templateUrl: 'templates/chat-detail.html',
-            controller: 'ChatDetailCtrl'
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html'
+          }
+        }
+      })
+
+      .state('tab.cart', {
+        url: '/cart',
+        views: {
+          'tab-cart': {
+            templateUrl: 'templates/tab-cart.html'
           }
         }
       })
@@ -79,13 +86,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         url: '/account',
         views: {
           'tab-account': {
-            templateUrl: 'templates/tab-account.html',
-            controller: 'AccountCtrl'
+            templateUrl: 'templates/tab-account.html'
           }
         }
       });
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
+    $urlRouterProvider.otherwise('/tab/home');
 
   });
